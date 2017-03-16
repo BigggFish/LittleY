312fe252303be01fceac536d551e1f2317b9e3e2
bigggfish <1063631103@qq.com>
2017-03-16 10:13:23 +0800

    增加启动页实现

--- app/src/main/java/com/bigggfish/littley/activity/SplashActivity.java
+++ app/src/main/java/com/bigggfish/littley/activity/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.bigggfish.littley.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
@@ -7,36 +9,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bigggfish.littley.R;
+import com.bigggfish.littley.util.Constant;
+
+import java.lang.ref.WeakReference;
 
 public class SplashActivity extends AppCompatActivity {
 
     private static final int TAG_GOTO_MAIN = 1;
-    private static final int TOTAL_TIME_IN_SPLASH = 3000;//ms
+    private static final int TOTAL_TIME_IN_SPLASH = 2000;//ms
 
-    private Handler handler = new Handler(){
+    static class MyHandler extends Handler{
+
+        private WeakReference<Activity> refActivity;
+        public MyHandler(Activity activity){
+            refActivity = new WeakReference<>(activity);
+        }
+
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
-                case TAG_GOTO_MAIN:
-                    gotoMainActivity();
-                    break;
+            final Activity activity = refActivity.get();
+            if(activity != null){
+                gotoMainActivity(activity);
             }
         }
-    };
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.NormalTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         initView();
     }
 
-    private void gotoMainActivity(){
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        SplashActivity.this.startActivity(intent);
-        SplashActivity.this.finish();
+    private void initView(){
+        new MyHandler(this).sendEmptyMessageDelayed(TAG_GOTO_MAIN, TOTAL_TIME_IN_SPLASH);
     }
 
-    private void initView(){
-        handler.sendEmptyMessageDelayed(TAG_GOTO_MAIN, TOTAL_TIME_IN_SPLASH);
+
+    ///////////////////////////static///////////////////////////////////////
+
+
+    private static void gotoMainActivity(Activity activity){
+        Intent intent = new Intent(activity, MainActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
     }
 }
